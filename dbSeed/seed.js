96fbe16dce2a17db1ab362fe0ebcb325adca5025
Michael Alaev <alaev.michael@gmail.com>
2017-06-11 22:25:09 +0300

    cleanup and added seed script to package.json

--- dbSeed/seed.js
+++ dbSeed/seed.js
@@ -9,33 +9,13 @@ const borrowersData = require('./borrowers');
 const librariansData = require('./librarians');
 require('../server/helpers/dbConnect').connect();
 
-//old
-// function seed(array, Collection) {
-  //   _.forEach(array, function(element, i, arr) {
-  //     const newElement = new Collection(element);
-  //     newElement.save()
-  //       .then(res => {);
-  //           console.log("now seeding copies");
-  //         }
-  //       })
-  //       .catch(err => {
-  //         console.log(err.errors);
-  //       });
-  //   });
-// }
-
 function seedCollection(array, col) {
   console.log('seeding ' + col.collection.collectionName);
   col.insertMany(array)
-    .then(v => console.log('added ' + v.length + ' ' + col.collection.collectionName));
+    .then(data => console.log('added ' + data.length + ' ' + col.collection.collectionName));
 }
 
-async function seedBooksAndCopies(dataBooks, dataCopies) {
-  // console.log("seeding books");
-  // let books = await Book.insertMany(dataBooks);//.then(res => seedCopies(copiesData));
-  // console.log(books.length + 'books', books);
-  // console.log("seeding copies");
-
+function seedCopies(dataBooks, dataCopies) {
   _.forEach(dataCopies, function (element, i, arr) {
     Book.findOne({ ISBN: element.ID.split("-")[0] })
       .then(book => {
@@ -43,7 +23,7 @@ async function seedBooksAndCopies(dataBooks, dataCopies) {
         copy.save()
           .then(() => book.copies.push(copy._id))
           .then(() => book.save())
-          .then(() => console.log('copy saved'))
+          .then(() => console.log('copy added'))
           .catch(err => console.log(err))
       })
       .catch(err => console.log(err))
@@ -55,22 +35,9 @@ seedCollection(librariansData, Librarian);
 seedCollection(booksData, Book);
 
 setTimeout(function() {
-  seedBooksAndCopies(booksData, copiesData);
+  seedCopies(booksData, copiesData);
   setTimeout(function() {
     process.exit(0);
   }, 1000);
 }, 1000);
-// Book.insertMany(booksData)
-//   .then(() => Borrower.insertMany(borrowersData))
-//   .then(() => seedCopies(copiesData))
-//   .then(() => Librarian.insertMany(librariansData))
-//   .catch(err => console.log(err))
-
-// Book.insertMany(booksData)
-//   .then(() => {
-//     console.log('done books');
-//     seedCollection(borrowersData, Borrower)
-//     seedCollection(librariansData, Librarian)
-//     seedCopies(copiesData);
-//   })
 
