8ab9982ad167e3e4fb3a628ebbc4a900e4efbd65
Michael Alaev <alaev.michael@gmail.com>
2017-06-11 20:55:17 +0300

    seed

--- dbSeed/seed.js
+++ dbSeed/seed.js
@@ -9,22 +9,65 @@ const borrowersData = require('./borrowers');
 const librariansData = require('./librarians');
 require('../server/helpers/dbConnect').connect();
 
-function seed(array, Collection) {
-  _.forEach(array, function(element) {
-    const newElement = new Collection(element);
-    newElement.save()
-      .then(res => {
-        console.log('element added to ' + Collection.collection.collectionName);
-      })
-      .catch(err => {
-        console.log(err.errors);
-      });
-  });
+//old
+// function seed(array, Collection) {
+  //   _.forEach(array, function(element, i, arr) {
+  //     const newElement = new Collection(element);
+  //     newElement.save()
+  //       .then(res => {);
+  //           console.log("now seeding copies");
+  //         }
+  //       })
+  //       .catch(err => {
+  //         console.log(err.errors);
+  //       });
+  //   });
+// }
+
+function seedCollection(array, col) {
+  console.log('seeding ' + col.collection.collectionName);
+  col.insertMany(array)
+    .then(v => console.log('added ' + v.length + ' ' + col.collection.collectionName));
 }
 
-seed(booksData, Book);
-seed(borrowersData, Borrower);
-seed(librariansData, Librarian);
+async function seedBooksAndCopies(dataBooks, dataCopies) {
+  console.log("seeding books");
+  let books = await Book.insertMany(dataBooks);//.then(res => seedCopies(copiesData));
+  console.log(books.length + 'books', books);
+  console.log("seeding copies");
+
+  _.forEach(dataCopies, function (element, i, arr) {
+    Book.findOne({ ISBN: element.ID.split("-")[0] })
+      .then(book => {
+        const copy = new Copy({ book: book._id, ID: element.ID, status: 'available' })
+        copy.save()
+          .then(() => book.copies.push(copy._id))
+          .then(() => book.save())
+          .then(() => console.log('copy saved'))
+          .catch(err => console.log(err))
+      })
+      .catch(err => console.log(err))
+    });
+}
+
+seedCollection(borrowersData, Borrower);
+seedCollection(librariansData, Librarian);
+// seedCollection(booksData, Book);
+seedBooksAndCopies(booksData, copiesData);
+
+// Book.insertMany(booksData)
+//   .then(() => Borrower.insertMany(borrowersData))
+//   .then(() => seedCopies(copiesData))
+//   .then(() => Librarian.insertMany(librariansData))
+//   .catch(err => console.log(err))
+
+// Book.insertMany(booksData)
+//   .then(() => {
+//     console.log('done books');
+//     seedCollection(borrowersData, Borrower)
+//     seedCollection(librariansData, Librarian)
+//     seedCopies(copiesData);
+//   })
 
 setTimeout(function() {
   process.exit(0);
