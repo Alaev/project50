267f58b5b63deac8444bb20e6e436a8b6e874a45
Michael Alaev <alaev.michael@gmail.com>
2017-06-16 10:52:01 -0800

    fixed lint

--- server/controllers/librarians.js
+++ server/controllers/librarians.js
@@ -1,12 +1,12 @@
 const Librarian = require('../models/Librarians');
 
 module.exports = {
-  index: async(req, res) => {
+  index: async (req, res) => {
     const librarians = await Librarian.find({}, '-_id');
     res.status(200).json(librarians);
   },
 
-  newLibrarian: async(req, res) => {
+  newLibrarian: async (req, res) => {
     try {
       const librarian = req.body;
 
@@ -17,62 +17,41 @@ module.exports = {
         email: librarian.email,
         password: librarian.password
       });
-
       const registeredLibrarian = await newLibrarian.save();
-      res.status(200).json(registeredLibrarian);
 
+      res.status(200).json(registeredLibrarian);
     } catch (error) {
       res.status(206).json('Unable to register a librarian');
     }
   },
 
-  getLibrarian: async(req, res) => {
-    const {
-      librarianID
-    } = req.params;
-    const librarian = await Librarian.findOne({
-      ID: librarianID
-    });
+  getLibrarian: async (req, res) => {
+    const { librarianID } = req.params;
+    const librarian = await Librarian.findOne({ ID: librarianID });
     res.status(200).json(librarian);
   },
 
-  replaceLibrarian: async(req, res) => {
+  replaceLibrarian: async (req, res) => {
     // enforce req.body must have all the fields
-    const {
-      librarianID
-    } = req.params;
+    const { librarianID } = req.params;
     const newLibrarian = req.body;
-    const result = await Librarian.findOneAndUpdate({
-      ID: librarianID
-    }, newLibrarian);
+    const result = await Librarian.findOneAndUpdate({ ID: librarianID }, newLibrarian);
 
     if (!result) {
-      res.status(404).json({
-        message: "Can't replaced librarian, librarian was not found"
-      });
+      res.status(404).json({ message: "Can't replaced librarian, librarian was not found" });
     } else {
-      res.status(200).json({
-        message: 'Great news, librarian was replaced'
-      });
+      res.status(200).json({ message: 'Great news, librarian was replaced' });
     }
   },
 
-  deleteLibrarian: async(req, res) => {
-    const {
-      librarianID
-    } = req.params;
-    const removed = await Librarian.findOneAndRemove({
-      ID: librarianID
-    });
+  deleteLibrarian: async (req, res) => {
+    const { librarianID } = req.params;
+    const removed = await Librarian.findOneAndRemove({ ID: librarianID });
 
     if (!removed) {
-      res.status(200).json({
-        message: "Can't delete librarian, librarian was not found"
-      });
+      res.status(200).json({ message: "Can't delete librarian, librarian was not found" });
     } else {
-      res.status(200).json({
-        message: 'Librarian was deleted!'
-      });
+      res.status(200).json({ message: 'Librarian was deleted!' });
     }
   }
 };
--- server/models/Librarians.js
+++ server/models/Librarians.js
@@ -1,6 +1,9 @@
-/* eslint no-useless-escape: 0*/
+/* eslint no-useless-escape: 0 */
+/* eslint func-names: ["off", "as-needed"]*/
+
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt-nodejs');
+
 const Schema = mongoose.Schema;
 
 const LibrarianSchema = new Schema({
@@ -37,31 +40,34 @@ const LibrarianSchema = new Schema({
   password: String
 });
 
-//can't use arrow function, need to keep the context of 'this'.
-LibrarianSchema.pre('save', function (next) {
+// can't use arrow function, need to keep the context of 'this'.
+LibrarianSchema.pre('save', function(next) {
   const librarian = this;
 
   if (!librarian.isModified('password')) {
     return next();
-  };
+  }
 
-  bcrypt.genSalt(10, (err, salt) => {
+  return bcrypt.genSalt(10, (err, salt) => {
     if (err) {
       return next(err);
     }
 
-    bcrypt.hash(librarian.password, salt, null, (err, hash) => {
+    return bcrypt.hash(librarian.password, salt, null, hash => {
       if (err) {
         return next(err);
       }
 
       librarian.password = hash;
-      next();
+      return next();
     });
   });
 });
 
 const Librarian = mongoose.model('librarian', LibrarianSchema);
+
+// Elad and Mor -> Please clarify why do we need to create index here?
+// can't we just use the objectID?
 Librarian.collection.createIndex({
   ID: 1
 });
