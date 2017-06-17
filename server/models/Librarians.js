9b69c882345b99d2a393f4a4f36dde697d2e26f8
Elad Mizrahi <men.log@gmail.com>
2017-06-17 19:42:18 +0300

    update

--- server/models/Librarians.js
+++ server/models/Librarians.js
@@ -5,34 +5,34 @@ const Schema = mongoose.Schema;
 
 const LibrarianSchema = new Schema({
   ID: {
-    type: String
-    // validate: {
-    //   validator: v => /[0-9]{9}/.test(v),
-    //   message: 'Invalid ID'
-    // },
-    // required: [true, 'ID is required']
+    type: String,
+    validate: {
+      validator: v => /[0-9]{9}/.test(v),
+      message: 'Invalid ID'
+    },
+    required: [true, 'ID is required']
   },
   name: {
     first: String,
     last: String
   },
   phone: {
-    type: String
-    // validate: {
-    //   validator: phoneInput => /^\d{10}$/.test(phoneInput),
-    //   message: 'Invalid phone number'
-    // }
+    type: String,
+    validate: {
+      validator: phoneInput => /^\d{10}$/.test(phoneInput),
+      message: 'Invalid phone number'
+    }
   },
   email: {
-    type: String
-    // validate: {
-    //   validator: emailInput =>
-    //     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-    //       emailInput
-    //     ),
-    //   message: 'Invalid email address'
-    // },
-    // required: [true, 'Email is required']
+    type: String,
+    validate: {
+      validator: emailInput =>
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+          emailInput
+        ),
+      message: 'Invalid email address'
+    },
+    required: [true, 'Email is required']
   },
   password: String
 });
